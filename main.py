3768bc8b921fef76de5173c23e36118d16b43d8f
lucasSimonelli <lucasp.simonelli@gmail.com>
2015-07-04 12:45:19 -0300

    Decision trees working

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from sklearn import tree
 import csv
+import sys
 
 class TestData(object):
     """Anemic class to hold test data"""
-    def __init__(self, userid, age_range, gender, merchantid):
+    def __init__(self, userid, age_range, gender, merchantid, label):
         super(TestData, self).__init__()
         self.userid = int(userid)
         if age_range == '':
@@ -11,12 +12,12 @@ class TestData(object):
         else:
             self.age_range = int(age_range)
         if gender == '':
-            self.age_range = 0
+            self.gender = 0
         else:
             self.gender = int(gender)
 
         self.merchantid = int(merchantid)
-
+        self.label = int(label)
     def toList():
         return [self.age_range, self.gender, self.merchantid]
  
@@ -39,10 +40,9 @@ def build_training_dataset(filename):
             inp = [int(data[1]), int(data[2]), int(data[3])]
             if (int(data[4])==0):
                 #Balance
-                if (numberOfOneTimers <= numberOfLoyals):
-                    ds[1].append(-1)
-                    numberOfOneTimers = numberOfOneTimers + 1;
-                    ds[0].append(inp)
+                ds[1].append(-1)
+                numberOfOneTimers = numberOfOneTimers + 1;
+                ds[0].append(inp)
             else:
                 ds[1].append(1)    
                 numberOfLoyals = numberOfLoyals + 1;  
@@ -52,21 +52,18 @@ def build_training_dataset(filename):
 
 # Por ahi esto no entra todo en memoria. Ver
 # Devuelve lista de TestData
-def buildTestData(userMerchantFile, userInfoFile):
-    userProfiles = {}
-    with open(userInfoFile, 'rb') as userInfoF:
-        reader = csv.reader(userInfoF)
-        for index, row in enumerate(reader):
-            if index == 0:
-                continue
-            userProfiles[row[0]] = row[1:]  
+def buildTestData(testData):
     returnData = []
-    with open(userMerchantFile, 'rb') as userMerchantF:
+    print "Building test data"
+    csv.field_size_limit(sys.maxsize)
+    with open(testData, 'rb') as userMerchantF:
         reader = csv.reader(userMerchantF)
         for index, row in enumerate(reader):
-            if index == 0:
+            if index == 0 or row[4] == '':
                 continue
-            returnData.append(TestData(row[0], userProfiles[row[0]][0], userProfiles[row[0]][1], row[1]))
+            if index == 30000:
+                break;
+            returnData.append(TestData(userid=row[0], age_range=row[1], gender=row[2], merchantid = row[3], label=row[4]))
     return returnData
 
 
@@ -74,20 +71,33 @@ def toList(testData):
     return testData.toList()
 
 def main():
-
-    
-
     training_dataset = build_training_dataset("./datasource.csv")
-    print len(training_dataset[1])
-    clf = tree.DecisionTreeClassifier(max_depth=10)
+    print "Number of records: {0}".format(len(training_dataset[1])+len(training_dataset[0]))
+    clf = tree.DecisionTreeClassifier(max_depth=8)
     clf = clf.fit(training_dataset[0],training_dataset[1])
 
-    testData = buildTestData("./heavyData/test_format1.csv","./heavyData/user_info_format1.csv")
+    testData = buildTestData("test_format2.csv")
+    totalError = 0.0
+    cont = 0
+    correct = 0
+    print "Testing tree with test data"
     for index, item in enumerate(testData):
-        if index > 100:
-            break;
-        print "{0},{1},{2}".format(item.userid, item.merchantid, clf.predict_proba([[item.age_range, item.gender, item.merchantid]]))
-
+        proba = clf.predict_proba([[item.age_range, item.gender, item.merchantid]])[0][1]
+        classification = clf.predict([[item.age_range, item.gender, item.merchantid]])[0]
+        error = 0.0
+        if item.label==1:
+            error+=1-proba
+            if classification==1:
+                correct+=1
+        else:
+            error+=proba
+            if classification==-1:
+                correct+=1
+        totalError+=error
+        cont+=1
+    print "Error total: {0}%".format(totalError*100 / cont)
+    print "Correctos: {0}. Incorrectos: {1}".format(correct, cont-correct)
+    print "Porcentaje correctos: {0}%".format(correct*100 / cont)
 
 if __name__ == "__main__":
     main()
